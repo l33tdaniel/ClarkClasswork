60b97e7c0f6f0b67116d71d40ca85ef204f8ac48
danielrockhurst <danielneugent@amdg.rockhursths.edu>
2022-01-07 13:10:19 -0600

    Adding in first functional component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 
+
+
 function App() {
-  return 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">TV Series List</h1>
+      </header>
+      <Intro />
+    </div>
+  );
 }
 
-export default App;
+function Intro(props) {
+  return (
+    <p className="App-intro">
+      Our first functional component
+    </p>
+  )
+}
+
+  export default App;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import App from './App';
 import ReactDOM from 'react-dom';
 
 
-const getCurrentDate = () => {
-  const date = new Date();
-  return date.toDateString();
-}
-const greeting = <h1>Hello World! Current date: {getCurrentDate()}</h1>;
-
-ReactDOM.render(greeting, document.getElementById('root'));
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // testing
