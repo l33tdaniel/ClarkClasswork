24777b3d643cd286fb48856912edbb7467589907
l33tDanelLocal <danielneugent@icloud.com>
2022-01-15 18:51:14 -0600

    ASMT 1

--- src/components/SeriesList/SeriesList.js
+++ src/components/SeriesList/SeriesList.js
@@ -3,9 +3,10 @@ import React from 'react';
 import './SeriesList.css';
 
 function SeriesList(props) {
+
     return (
-        <div>
-            <ul className="series-list">
+        <div className="show-list-container">
+            <ul className="show-list">
                 {props.list.map(series => (
                     <SeriesListItem series={series} key={series.show.id} />
                 ))}
@@ -14,9 +15,30 @@ function SeriesList(props) {
     )
 }
 
-function SeriesListItem({series}) {
+function SeriesListItem({ series }) {
+    const showID = series.show.id;
+    const showName = series.show.name;
+    const showSummary = series.show.summary;
+    const genres = series.show.genres.join(', ');
+    let showImage = 'https://place-hold.it/107x150';
+    let showNetwork = '';
+    try { showImage = series.show.image.medium; } catch(e) { }
+    try { showNetwork = series.show.network.name; } catch(e) { }
     return (
-        <li key={series.show.id}>{series.show.name}</li>
+        <li key={showID}>
+            <a href={"/show/" + showID + "/seasons"}>
+                <div className="ib w20">
+                    <img src={showImage} alt={showName} />
+                </div>
+                <div className="ib w80">
+                    <div>{showName}</div>
+                    <div>{showNetwork.length > 0 ? "Network: " + showNetwork : ""}</div>
+                    <div>{genres.length > 0 ? "Genres: " + genres : ""}</div>
+                    <div dangerouslySetInnerHTML={{__html: showSummary}}></div>                    
+                </div>
+            </a>
+        </li>
     )
 }
+
 export default SeriesList;
